41688ac274367ab0675f3c94031c9ca323260eba
MrClemGIt <mrclem.nw+GitHub@gmail.com>
2014-10-09 18:07:33 +0200

    add some stats, colors ...

--- bedditdata.php
+++ bedditdata.php
@@ -61,24 +61,43 @@ $data = json_decode($result);
 echo '<table border="1" width="900">';
 echo '<tbody>';
 
-		echo '<tr bgcolor="white" bordercolor="green">';
+		echo '<tr bgcolor="#A9F5F2" bordercolor="green">';
         echo '<td align="center">Date </td>';
         echo '<td align="center">Sleep duration (h)</td>';
 		echo '<td align="center">Tags</td>';
 		echo '<td align="center">Sleep latency (min)</td>';
 		echo '<td align="center">Resting Heart rate </td>';
 		echo '<td align="center" >Score amount of sleep</td>';
-        echo '<td align="center">Score Sleep latency</td>';
+        //echo '<td align="center">Score Sleep latency</td>';
 		echo '<td align="center">Away Episode Count</td>';
 		
         echo '</tr>';
+
+$resting_heart_rate_sum=0;
+$stage_duration_S_sum=0;
+$number_of_sleeps=0;
+$sleep_with_error=0;
 foreach($data as $record)
 {
 		
-		echo '<tr>';
+		if ($record->properties->stage_duration_S == 0)
+		{
+			echo '<tr bgcolor="#F8E0E0">';
+			$sleep_with_error++; 
+		}
+		else
+		{
+			$number_of_sleeps++;
+			echo '<tr>';
+		}
+
         echo '<td align="center">' . $record->date . '</td>';
-        echo '<td align="center">' . round($record->properties->stage_duration_S/3600,2) . '</td>';
-		
+        
+        $hour = $record->properties->stage_duration_S/3600;
+        $min = round((round($record->properties->stage_duration_S/3600,2)-intval($hour))*60,0);
+        
+        echo '<td align="center">' . intval($hour) .':'. sprintf("%02d", $min) .'</td>';
+		$stage_duration_S_sum += round($record->properties->stage_duration_S/3600,2);
 		
 		echo '<td align="center">';
 		
@@ -99,14 +118,15 @@ foreach($data as $record)
 		}
 		if (isset($record->properties->sleep_latency)==true)
 		{
-			echo '<td align="center">' . round($record->properties->resting_heart_rate,0) . '</td>';		
+			echo '<td align="center">' . round($record->properties->resting_heart_rate,0) . '</td>';
+			$resting_heart_rate_sum+= $record->properties->resting_heart_rate;		
 		}
 		else
 		{
 			echo '<td align="center">NA</td>';
 		}
 		echo '<td align="center">' . $record->properties->score_amount_of_sleep . '</td>';
-        echo '<td align="center">' . $record->properties->score_sleep_latency . '</td>';
+       //echo '<td align="center">' . $record->properties->score_sleep_latency . '</td>';
 		echo '<td align="center">' . $record->properties->away_episode_count . '</td>';		
         echo '</tr>';
 	
@@ -116,6 +136,19 @@ foreach($data as $record)
 	
 
 }
+echo '<tr bgcolor="#A9F5F2" bordercolor="green">';
+        echo '<td align="center">Sleep Duration Mean </td>';
+        echo '<td align="center">Resting Heart Rate Mean</td>';
+		echo '<td align="center">Number of Sleeps</td>';
+		echo '<td align="center">Sleep in Error (min)</td>';
+		echo '</tr>';
+echo '<tr>';
+echo '<td align="center">' . round($stage_duration_S_sum/$number_of_sleeps,2) . '</td>';
+echo '<td align="center">' . round($resting_heart_rate_sum/$number_of_sleeps,2) . '</td>';
+echo '<td align="center">' . $number_of_sleeps . '</td>';
+echo '<td align="center">' . $sleep_with_error . '</td>';
+echo '</tr>';
+
 echo '</tbody>';
 echo '</table>';
 
